1b51ff2aecb6e6fad18c7a5fcf694e9a53cf2c96
MichaelPrigmore <prig1103@stthomas.edu>
2020-10-16 14:41:44 -0500

    failed test

--- ISBNValidator/src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
+++ ISBNValidator/src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
@@ -1,7 +1,12 @@
 package com.virtualpairprogrammers.isbntools;
 
-public class ValidateISBN 
+public class ValidateISBN
 {
-	
-	
+
+	public boolean checkISBN(int i)
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 }
--- ISBNValidator/test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
+++ ISBNValidator/test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
@@ -4,11 +4,15 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class ValidateISBNTest {
+public class ValidateISBNTest
+{
 
 	@Test
-	public void check_a_valid_ISBN() {
-		fail();
+	public void check_a_valid_ISBN()
+	{
+		ValidateISBN validator = new ValidateISBN();
+		boolean result = validator.checkISBN(140449116);
+		assertTrue(result);
 	}
 
 }
