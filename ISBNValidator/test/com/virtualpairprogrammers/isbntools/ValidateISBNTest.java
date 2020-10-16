b3b7e9770c5c1fee3b7199aa634d8c6bb028f67d
MichaelPrigmore <prig1103@stthomas.edu>
2020-10-16 14:24:35 -0500

    failed test

--- ISBNValidator/test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
+++ ISBNValidator/test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
@@ -1,12 +1,14 @@
 package com.virtualpairprogrammers.isbntools;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 public class ValidateISBNTest {
 
 	@Test
-	public void test() {
-
+	public void check_a_valid_ISBN() {
+		fail();
 	}
 
 }
