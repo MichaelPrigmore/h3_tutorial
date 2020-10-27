d6d59218e69fc1e3e8b9af92dfe72cb5067e3332
MichaelPrigmore <prig1103@stthomas.edu>
2020-10-27 14:08:42 -0500

    Last commit for ch 3 of video

--- ISBNValidator/src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
+++ ISBNValidator/src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
@@ -3,13 +3,23 @@ package com.virtualpairprogrammers.isbntools;
 public class ValidateISBN
 {
 
-	public boolean checkISBN(int isbn)
+	public boolean checkISBN(String isbn)
 	{
-		if (isbn == 140449116)
+		int total = 0;
+
+		for (int i = 0; i < 10; i++)
+		{
+			total += isbn.charAt(i) * (10 - i);
+		}
+
+		if (total % 11 == 0)
 		{
 			return true;
 		}
-		return false;
+		else
+		{
+			return false;
+		}
 	}
 
 }
--- ISBNValidator/test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
+++ ISBNValidator/test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
@@ -11,15 +11,17 @@ public class ValidateISBNTest
 	public void check_a_valid_ISBN()
 	{
 		ValidateISBN validator = new ValidateISBN();
-		boolean result = validator.checkISBN(140449116);
-		assertTrue(result);
+		boolean result = validator.checkISBN("0140449116");
+		assertTrue("first value", result);
+		result = validator.checkISBN("0140177396");
+		assertTrue("second value", result);
 	}
 
 	@Test
 	public void check_an_invalid_ISBN()
 	{
 		ValidateISBN validator = new ValidateISBN();
-		boolean result = validator.checkISBN(140449117);
+		boolean result = validator.checkISBN("0140449117");
 		assertFalse(result);
 	}
 
